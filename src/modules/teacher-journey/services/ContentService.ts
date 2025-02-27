3945ef5786949f19642de42cfa24dc0bb904f485
albericobenhur <sudbenhur@gmail.com>
2025-02-27 12:20:27 -0300

    chore: filtros componentizado

--- src/modules/teacher-journey/services/ContentService.ts
+++ src/modules/teacher-journey/services/ContentService.ts
@@ -1,66 +1,66 @@
-import BaseService from "@/services/BaseService";
-import type { Content } from "@prisma/client";
-import { formatISO } from "date-fns"
+import type { Content } from '@prisma/client'
+import BaseService from '@/services/BaseService'
+import { formatISO } from 'date-fns'
 
-const table = "content" as const
+const table = 'content' as const
 
 export default class ContentService extends BaseService<Content> {
-    constructor() {
-        super(table);
+  constructor() {
+    super(table)
+  }
+
+  async listContentTeacherId(teacherId: string) {
+    const { data, error } = await this.client
+      .from('content')
+      .select('*')
+      .eq('teacherId', teacherId)
+
+    if (error) {
+      throw new Error(`Erro ao encontrar conteúdo: ${error.message}`)
     }
-    async listContentTeacherId(teacherId: string) {
-        const { data, error } = await this.client
-            .from("content")
-            .select("*")
-            .eq("teacherId", teacherId);
-
-        if (error) {
-            throw new Error(`Erro ao encontrar conteúdo: ${error.message}`);
-        }
-        if (!data) {
-            throw new Error("Nenhum conteúdo encontrado");
-        }
-
-        return data;
+    if (!data) {
+      throw new Error('Nenhum conteúdo encontrado')
     }
 
-    async listContentByToday(teacherId: string, date: string) {
-        const startOfDay = new Date(date);
-        startOfDay.setHours(0, 0, 0, 0);
-        const endOfDay = new Date(date);
-        endOfDay.setHours(23, 59, 59, 999);
+    return data
+  }
 
-        const { data, error } = await this.client
-            .from("content")
-            .select(`
+  async listContentByToday(classroomId: string, date: string) {
+    const startOfDay = new Date(date)
+    startOfDay.setHours(0, 0, 0, 0)
+    const endOfDay = new Date(date)
+    endOfDay.setHours(23, 59, 59, 999)
+
+    const { data, error } = await this.client
+      .from('content')
+      .select(`
             id,
             date,
             description,
-            classroom: classroomId (name),
+            classroom: classroomId (id, name),
             disciplines: content_discipline (disciplineId (id, name)),
             bnccs: content_bncc (bnccId (id, code, objective))
             `)
-            .eq("teacherId", teacherId)
-            .eq("date", date);
-        // .gte("date", formatISO(startOfDay))
-        // .lte("date", formatISO(endOfDay))
+      .eq('date', date)
+      .eq('classroomId', classroomId)
+    // .eq('teacherId', teacherId) // @TODO: teacherId não é util para ser um parametro do filtro ( pode ser que o professor mude durante o decorrer do ano )
 
-        console.log("log ", data);
+    console.log('log ', data)
 
-        if (error) {
-            throw new Error(`Erro ao encontrar conteúdo: ${error.message}`);
-        }
-        if (!data || data.length === 0) {
-            throw new Error("Nenhum conteúdo encontrado");
-        }
-
-        const contentMap = data.map((item: any) => ({
-            classroom: item.classroom.name,
-            date: item.date,
-            description: item.description,
-            disciplines: item.disciplines.map((d: any) => d.disciplineId.name),
-            bnccs: item.bnccs.map((b: any) => b.bnccId.code)
-        }));
-        return contentMap;
+    if (error) {
+      throw new Error(`Erro ao encontrar conteúdo: ${error.message}`)
     }
+    if (!data || data.length === 0) {
+      throw new Error('Nenhum conteúdo encontrado')
+    }
+
+    const contentMap = data.map((item: any) => ({
+      classroom: item.classroom.name,
+      date: item.date,
+      description: item.description,
+      disciplines: item.disciplines,
+      bnccs: item.bnccs,
+    }))
+    return contentMap
+  }
 }
