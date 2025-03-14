864a28fe3d3ce3a27add468835cb05ea02d2b731
Renandreolle <renandreolle@hotmail.com>
2025-03-14 11:34:19 -0300

    feat: adicionado serviço de regra de avaliação

---
+++ src/modules/teacher-journey/services/EvaluationRuleService.ts
@@ -0,0 +1,26 @@
+import type { EvaluationRule } from '@prisma/client'
+import BaseService from '@/services/BaseService'
+
+const table = 'evaluationRule' as const // Modifique para sua tabela
+
+export default class EvaluationRuleService extends BaseService<EvaluationRule> {
+  constructor() {
+    super(table) // Passando o nome da tabela para a classe base
+  }
+
+  async getRulesFromCourse(courseId) {
+    const { data, error } = await this.client
+      .from(table)
+      .select('*')
+      .eq('courseId',courseId)
+
+    if (error) {
+      throw new Error(`Erro ao trazer as regras: ${error.message}`)
+    }
+    if (!data) {
+      throw new Error('Nenhuma regra encontrada')
+    }
+
+    return data
+  }
+}
