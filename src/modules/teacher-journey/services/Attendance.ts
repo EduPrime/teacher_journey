d65a6501b75f4160bcd0bee76e896e3f36ae9a7a
alan-cleveston <alan.cleveston@softagon.com.br>
2025-03-15 13:30:13 -0300

    chore: Adicionando service de criar attendance e numMissed

--- src/modules/teacher-journey/services/Attendance.ts
+++ src/modules/teacher-journey/services/Attendance.ts
@@ -4,23 +4,76 @@ import BaseService from '@/services/BaseService'
 const table = 'attendance' as const
 
 export default class EnrollmentService extends BaseService<Attendance> {
-    constructor() {
-        super(table)
-    }
-    async listAttendanceAll(enrollmentId: string | null) {
-        const { data, error } = await this.client
-            .from(table)
-            .select('*')
-            .eq('enrollmentId', enrollmentId)
+  constructor() {
+    super(table)
+  }
 
-        if (error) {
-            throw new Error(`Erro listar todas as frequencias: ${error.message}`)
+  async createAttendance(frequencies: Attendance[]) {
+    try {
+      const attendanceRecords = []
+
+      for (const frequency of frequencies) {
+        // Insere o registro de frequência
+        const { data: attendanceData, error: attendanceError } = await this.client
+          .from(table)
+          .insert([{
+            date: frequency.date,
+            studentId: frequency.studentId,
+            enrollmentId: frequency.enrollmentId,
+            disciplineId: frequency.disciplineId,
+            justificationId: frequency.justificationId,
+            stageId: frequency.stageId,
+            schoolId: frequency.schoolId,
+            updatedBy: frequency.updatedBy,
+            tenantId: frequency.tenantId,
+          }])
+          .select()
+          .single()
+
+        if (attendanceError) {
+          throw new Error(`Erro ao inserir frequência: ${attendanceError.message}`)
         }
-        if (!data) {
-            throw new Error('Nenhuma frequencia encontrada')
+
+        const attendanceId = attendanceData.id
+
+        // Insere os registros relacionados na tabela numMissed
+        const numMissedRecords = frequency.numMissed.map(frequency => ({
+          attendanceId,
+          name: frequency.name,
+          absent: frequency.absence,
+        }))
+
+        const { error: numMissedError } = await this.client
+          .from('numMissed')
+          .insert(numMissedRecords)
+
+        if (numMissedError) {
+          throw new Error(`Erro ao inserir registros de ausência: ${numMissedError.message}`)
         }
 
-        return data
+        attendanceRecords.push(attendanceData)
+      }
+
+      return attendanceRecords
+    }
+    catch (error) {
+      throw new Error(`Erro ao inserir frequência: ${error.message}`)
+    }
+  }
+
+  async listAttendanceAll(enrollmentId: string | null) {
+    const { data, error } = await this.client
+      .from(table)
+      .select('*')
+      .eq('enrollmentId', enrollmentId)
+
+    if (error) {
+      throw new Error(`Erro listar todas as frequencias: ${error.message}`)
+    }
+    if (!data) {
+      throw new Error('Nenhuma frequencia encontrada')
     }
 
+    return data
+  }
 }
