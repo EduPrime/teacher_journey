70e27cc50ef95b90a23d5ec93ee10eed5b75e31f
Renandreolle <renandreolle@hotmail.com>
2025-04-02 14:32:53 -0300

    fix: ajuste de código para não salvar várias modificações da mesma nota

--- src/modules/teacher-journey/views/TeacherConceptualEvaluation.vue
+++ src/modules/teacher-journey/views/TeacherConceptualEvaluation.vue
@@ -69,13 +69,15 @@ function compareGrades(oldGrades: Grades[], newGrades: Grades[], updatedGrades: 
     if (oldValue !== newGrade.value) {
       hasChanged = true
 
-      const exists = updatedGrades.some(
+      const existingGrade = updatedGrades.find(
         updated =>
-          updated.grade === newGrade.value &&
           updated.conceptualGradeId === newGrade.gradeId &&
           updated.thematicUnitId === newGrade.thematicUnitId
       )
-      if (!exists) {
+
+      if (existingGrade) {
+        existingGrade.grade = newGrade.value
+      } else {
         updatedGrades.push({
           grade: newGrade.value,
           conceptualGradeId: newGrade.gradeId,
